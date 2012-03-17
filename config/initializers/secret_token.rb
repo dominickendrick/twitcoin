be12199e485c0fa6021128152237e976a546f7a7
Dominic Kendrick <dkendric@amnesty.org>
2012-03-17 13:55:27 +0000

    initail rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitcoin::Application.config.secret_token = 'd14949c21742d7d035b75183a02e3152d5135511cd04252fb8f1af8a9b590a1fe8d572e57b8f142d195eca76bfcf1143d5b8554b8deb4ad4e5af7c3bbaeb3a96'
